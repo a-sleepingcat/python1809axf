babf6514241940305aad8268f9b07fef290d6ed6
a-sleepingcat <1963573257@qq.com>
2018-11-02 21:21:18 -0700

    框架

---
+++ axf/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+# 基础类
+class Base(models.Model):
+    # 图片
+    img = models.CharField(max_length=100)
+    # 名称
+    name = models.CharField(max_length=100)
+    # 商品编号
+    trackid = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
---
+++ axf/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request): #首页
+    #轮播图数据
+    pass
+
+
+def market(request):
+    return None
+
+
+def cart(request):
+    return None
+
+
+def mine(request):
+    return None
