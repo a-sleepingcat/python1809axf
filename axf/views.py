b4c7297793f12b064a3fca0a39aeb3b4f376da33
a-sleepingcat <1963573257@qq.com>
2018-11-05 22:35:19 -0800

    首页补充完成

--- axf/views.py
+++ axf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from axf.models import Wheel, Nav, Mustbuy
+from axf.models import Wheel, Nav, Mustbuy, Shop, Mainshow
 
 
 def home(request): #首页
@@ -13,10 +13,25 @@ def home(request): #首页
 
     # 每日必购
     mustbuys = Mustbuy.objects.all()
+
+    # 商品部分
+    shoplist = Shop.objects.all()
+    shophead = shoplist[0]
+    shoptab = shoplist[1:3]
+    shopclass = shoplist[3:7]
+    shopcommend = shoplist[7:11]
+
+    # 商品主体内容
+    mainshows = Mainshow.objects.all()
     data = {
         'wheels':wheels,
         'navs':navs,
         'mustbuys':mustbuys,
+        'shophead':shophead,
+        'shoptab':shoptab,
+       'shopclass':shopclass,
+        'shopcommend':shopcommend,
+        'mainshows':mainshows,
     }
 
     return render(request,'home/home.html',context=data)
